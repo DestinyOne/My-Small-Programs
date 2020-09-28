c07be10095658a44fca333957c8e688e6f332ca9
Weirong Yuan <yuan161@purdue.edu>
2020-09-27 23:42:17 -0400

    Update GroupRP.py

--- GroupRP.py
+++ GroupRP.py
@@ -8,45 +8,7 @@ BufferList = ["Crew4", "Crew2", "Crew5", "Crew1"] # The first will be lastest
 
 testiter = 100000
 
-def TestResult(CrewList:list, BufferList:list, testiter:int):
-
-  n = len(CrewList)
-  nb = len(BufferList)
-
-  RemainList = []
-
-  for i in CrewList:
-    if i not in BufferList:
-      RemainList.append(i)
-
-  nr = len(RemainList)
-
-  if nb + nr != n:
-    raise ValueError("Wrong BufferList!\n")
-
-  BufferWeight = np.linspace(0.0, 1.0, nb, endpoint=False)
-  RemainWeight = np.ones(nr)
-
-  Weights = np.concatenate((BufferWeight, RemainWeight))
-  ReList = BufferList + RemainList
-
-  SumWeight = np.sum(Weights)
-
-  Score = np.zeros(n)
-
-  for i in range(testiter):
-    MyRand = random.uniform(0.0, SumWeight)
-    for idx, ele in np.ndenumerate(Weights):
-      MyRand -= ele
-      if MyRand < 0.0:
-        Score[idx] += 1
-        break
-
-  Score /= testiter
-
-  print(list(zip(ReList, Score)))
-
-def ThisResult(CrewList:list, BufferList:list):
+def RandomPickOld(CrewList:list, BufferList:list):
 
   n = len(CrewList)
   nb = len(BufferList)
@@ -74,11 +36,47 @@ def ThisResult(CrewList:list, BufferList:list):
   for idx, ele in np.ndenumerate(Weights):
     MyRand -= ele
     if MyRand < 0.0:
-      # print(ReList[idx[0]])
       return ReList[idx[0]]
       break
 
-def TestResult2(CrewList:list, BufferList:list, testiter:int):
+def RandomPick(CrewList:list, BufferList:list):
+
+  n = len(CrewList)
+  nb = len(BufferList)
+  BufferWeight = np.linspace(0.0, 1.0, nb, endpoint=False)
+  Weights = np.ones(n)
+
+  for i, x in enumerate(BufferList):
+    idx = CrewList.index(x)
+    Weights[idx] = BufferWeight[i]
+
+  SumWeight = np.sum(Weights)
+
+  MyRand = random.uniform(0.0, SumWeight)
+  for idx, ele in np.ndenumerate(Weights):
+    MyRand -= ele
+    if MyRand < 0.0:
+      return CrewList[idx[0]]
+      break
+
+def RandomPick2(CrewList:list, BufferList:list):
+
+  n = len(CrewList)
+  nb = len(BufferList)
+  BufferWeight = np.linspace(0.0, 1.0, nb, endpoint=False)
+  Weights = np.ones(n)
+
+  for i, x in enumerate(BufferList):
+    idx = CrewList.index(x)
+    Weights[idx] = BufferWeight[i]
+
+  while True:
+    Randi = random.randrange(n)
+    MyRand = random.random()
+    if MyRand < Weights[Randi]:
+      return CrewList[Randi]
+
+def TestResult(CrewList:list, BufferList:list, testiter:int, RPFun:object):
 
   n = len(CrewList)
   nb = len(BufferList)
@@ -104,10 +102,13 @@ def TestResult2(CrewList:list, BufferList:list, testiter:int):
   Score = np.zeros(n)
 
   for i in range(testiter):
-    Winner = ThisResult(CrewList, BufferList)
+    Winner = RPFun(CrewList, BufferList)
     idx = ReList.index(Winner)
     Score[idx] += 1
 
   Score /= testiter
 
   print(list(zip(ReList, Score)))
+
+# TestResult(CrewList, BufferList, testiter, RandomPick2)
+print(RandomPick2(CrewList,BufferList))
